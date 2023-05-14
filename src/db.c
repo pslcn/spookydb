c36140a91a9a4d3ea4bcf4f26d41ade00383b0dd
pslcn <gregorsrussellcobb@gmail.com>
2023-05-14 08:02:35 +0100

    basic non-blocking I/O

--- src/db.c
+++ src/db.c
@@ -11,70 +11,122 @@
 #include <syslog.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
 
-#define BUFFSIZE 1024
-#define LENGTH(X) (sizeof X / sizeof X[0])
-
-typedef struct {
-	int fd;
-	char chunk_buff[BUFFSIZE];
-	int chunk_buff_len;
-	int chunk_buff_used; /* Could use to represent state */
-	int is_sending;
-} file_conn_t;
-
-int fd_set_nb(int fd) /* Set FD to non-blocking */
+int fd_set_non_blocking(int fd)
 {
-	int flags;
-#if defined(O_NONBLOCK)
-	if ((flags = fcntl(fd, F_GETFL, 0)) == -1)
-		flags = 0;
-	return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-#else 
-	flags = 1;
-	return ioctl(fd, FIOBIO, &flags);
-#endif
+	int flags = fcntl(fd, F_GETFL, 0);
+	if (flags == -1) 
+		return 1;
+	flags |= O_NONBLOCK;
+
+	return (fcntl(fd, F_SETFL, flags) == 0) ? 0 : 1;
 }
 
-void send_file(char *filename, int conn_fd, file_conn_t *file_conn)
+/* Non-blocking server socket FD */
+int create_serv_sock(int *serv_fd, struct sockaddr_in *servaddr) 
 {
-	file_conn->fd = open(filename, O_RDONLY);
-	if (file_conn->fd < 0)
-		exit(1); /* Open failed */
-	file_conn->chunk_buff_len = 0;
-	file_conn->chunk_buff_used = 0;
-	file_conn->is_sending = 1;
+	*serv_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (*serv_fd == -1)
+		exit(1);
+	fd_set_non_blocking(*serv_fd);
+
+	bzero(servaddr, sizeof(*servaddr));
+	(*servaddr).sin_family = AF_INET;
+	(*servaddr).sin_addr.s_addr = htonl(INADDR_ANY);
+	(*servaddr).sin_port = htons(8080);
+	if ((bind(*serv_fd, (struct sockaddr *)servaddr, sizeof(*servaddr))) != 0)
+		exit(1);
+
+	return 0;
 }
 
-int handle_io(file_conn_t *file_conn)
+/* Event loop uses poll.h */
+int main(void) 
 {
-	int nwrite;
+	int serv_fd;
+	struct sockaddr_in servaddr;
+	struct pollfd net_fds[32] = { 0 };
+	int nfds = 1;
+	int conn_fd = 0;
 
-	if (file_conn->is_sending == 0)
-		return 2;
+	create_serv_sock(&serv_fd, &servaddr);
+	if (listen(serv_fd, 32) < 0)
+		exit(1);
 
-	if (file_conn->chunk_buff_used == file_conn->chunk_buff_len) {
-		file_conn->chunk_buff_len = read(file_conn->fd, file_conn->chunk_buff, BUFFSIZE);
-		if (file_conn->chunk_buff_len == 0) { /* Finished reading file */
-			close(file_conn->fd);
-			close(conn_fd);
-			file_conn->is_sending = 0;
-			return 1;
+	net_fds[0].fd = serv_fd;
+	net_fds[0].events = POLLIN;
+
+	while (1) {
+		if (poll(net_fds, nfds, (5000)) > 0) {
+			/* Accept connection */
+			if (net_fds[0].revents & POLLIN) { 
+				struct sockaddr_in cli;
+				int addrlen = sizeof(cli);
+				conn_fd = accept(serv_fd, (struct sockaddr *)&cli, &addrlen);
+				if (conn_fd < 0)
+					break;
+
+				fprintf(stdout, "Accepted %s\n", inet_ntoa(cli.sin_addr));
+				for (size_t i = 1; i < 32; ++i) {
+					if (net_fds[i].fd <= 0) {
+						net_fds[i].fd = conn_fd;
+						net_fds[i].events = POLLIN;
+						nfds += 1;
+						break;
+					}
+				}
+
+				/*
+				while (conn_fd != -1) {
+					struct sockaddr_in cli;
+					int addrlen = sizeof(cli);
+					conn_fd = accept(serv_fd, (struct sockaddr *)&cli, &addrlen);
+					if (conn_fd < 0) 
+						break;
+
+					printf("Accepted %s\n", inet_ntoa(cli.sin_addr));
+					for (size_t i = 1; i < 32; ++i) {
+						if (net_fds[i].fd <= 0) {
+							net_fds[i].fd = conn_fd;
+							net_fds[i].events = POLLIN;
+							nfds += 1;
+							break;
+						}
+					}
+				}
+				*/
+			}
+
+			/* Check existing connections */
+			for (size_t i = 1; i < 32; ++i) {
+				if (net_fds[i].fd > 0 && net_fds[i].revents & POLLIN) {
+					printf("Closing net_fds[%d].fd (%d)\n", i, net_fds[i].fd);
+					close(net_fds[i].fd);
+					net_fds[i].fd = -1;
+				}
+			}
+
+			/* Clean up array */
+			for (size_t i = 1; i < 32; ++i) {
+				if (net_fds[i].fd < 0) {
+					net_fds[i].fd = 0;
+					net_fds[i].events = 0;
+					net_fds[i].revents = 0;
+					nfds -= 1;
+				}
+			}
 		}
-		file_conn->chunk_buff_used = 0;
 	}
 
-	assert(file_conn->chunk_buff_len > file_conn->chunk_buff_used);
-	nwrite = write(conn_fd, file_conn->chunk_buff + file_conn->chunk_buff_used, file_conn->chunk_buff_len - file_conn->chunk_buff_used);
-	if (nwrite < 0)
-		exit(1); /* Write failed */
-	file_conn->chunk_buff_used += nwrite;
+	/* Close FDs */
+	for (size_t i = 0; i < 32; ++i) {
+		if (net_fds[i].fd >= 0)
+			close(net_fds[i].fd);
+	}
 
 	return 0;
 }
 
-int main(void)
-{
-	int sockfd;
-	return 0;
-}
